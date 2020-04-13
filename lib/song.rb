c7ecb5ff30119f6a2e5ed55ddd271ce85a8601aa
Taulant Ibraj <tibraj94@gmail.com>
2020-04-13 16:41:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,4 +44,12 @@ class Song
   def self.alphabetical
     @@all.sort_by {|song| song.name}
   end
+
+  def self.new_from_filename("artist_name - song_name.mp3")
+    str = "artist_name - song_name.mp3"
+    filename = str.chomp(".mp3")
+    new_file = filename.split("-")
+    song.artist_name = new_file[0]
+    song.name = new_file[1]
+  end
 end
